fc0ad5426c9a28c46b49c7368d659562d251dbdb
Steven Fongue <sfongue@gmail.com>
2018-03-01 11:10:19 +0100

    Exercise 1 done

--- src/exercise1.js
+++ src/exercise1.js
@@ -3,11 +3,18 @@ const chalk = require('chalk');
 console.log(chalk.cyan('\nEXERCISE 1 LOADED\n'));
 
 const partition = (collection, predicate) => {
+  if (collection.length) {
+    return [
+      collection.filter( value => predicate(value)), 
+      collection.filter(value => !predicate(value))
+    ];
+  } else {
+    return [[],[]];
+  }
 
-  // Your future job begins here ...
-}
+};
 
 module.exports = {
   partition: partition,
   run: partition
-}
+};
